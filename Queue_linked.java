ae35082a550bb9c1a13ee4c431f95643d748bd30
pavan333c <nagapa1pinisetti@gmail.com>
2023-07-19 19:28:15 +0530

    Queu_linked

---
+++ Queue_linked.java
@@ -0,0 +1,78 @@
+import java.util.Scanner;
+
+import javax.annotation.processing.SupportedOptions;
+
+class Node{
+    int data;
+    Node next;
+}
+public class Queue_linked {
+    Node front = null;
+    Node rear = null;
+    void enqueue(){
+
+        Node newnode = new Node();
+         System.out.println("Enter Data:");
+        Scanner sc = new Scanner(System.in);
+        newnode.data = sc.nextInt();
+        if(front == null){
+            front = newnode;
+        }
+        else{
+            newnode.next = front;
+            front = newnode;
+        }
+    }
+    void dequeue(){
+        // front = front.next;
+        System.out.println("OKK!");
+        if(rear == null){
+            rear = front ;
+            System.out.println("OK!");
+        }
+        else{
+            
+            int ele = rear.data;
+            rear = rear.next;
+            System.out.println("Data ::"+ele);
+            
+        }
+    }
+    void display(){
+        Node temp = front;
+        while(temp != null){
+            System.out.println(temp.data+"->");
+            temp = temp.next;
+        }
+    }
+    public static void main(String[] args) {
+        Queue_linked obj = new Queue_linked();
+
+        int ch = 0;
+
+        while(true){
+            System.out.println(" ");
+            System.out.println("----Menu----");
+            System.out.println("1.Enqueue\n2.Dequeue\n3.Display\n4.Exit\n");
+            System.out.println("Enter Your Choice :");
+            Scanner sc = new Scanner(System.in);
+            ch = sc.nextInt();
+
+            switch(ch){
+                case 1: 
+                        obj.enqueue();
+                        break;
+                case 2:
+                        obj.dequeue();
+                        break;
+                case 3:
+                        obj.display();
+                        break;
+                case 4: System.exit(0);
+                        break;
+                default:System.out.println("Enter Correct Number");
+                        break;
+            }
+        }
+    }
+}
